2aab3d2a32ab03886a4c0237f0e692904b8642fe
Tasya <122555509+tas219@users.noreply.github.com>
2025-11-07 15:16:30 -0500

    Create CheckoutReceipt Entity and Repository

---
+++ libraq/src/main/java/com/example/libraq/repository/CheckoutReceiptRepository.java
@@ -0,0 +1,15 @@
+package com.example.libraq.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.libraq.model.CheckoutReceipt;
+
+public interface CheckoutReceiptRepository extends JpaRepository<CheckoutReceipt, Long> {
+
+    List<CheckoutReceipt> findByUserId(Long userId);
+
+    List<CheckoutReceipt> findByBookId(Long bookId);
+
+}
